62f10aa9fa5beb047c93baf7aee85b5228011b40
AlexMoreR <amoreanor.2024@gmail.com>
2025-04-16 16:34:42 -0500

    add: prisma 2

--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { WebhookController } from './webhook.controller';
 import { WebhookService } from './webhook.service';
+import { PrismaService } from 'src/prisma.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [WebhookController],
-  providers: [WebhookService]
+  providers: [WebhookService, PrismaService]
 })
 export class WebhookModule {}
