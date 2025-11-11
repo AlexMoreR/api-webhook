1441972a08d2cecfc1cd056f7243248b7213d3ea
Eros Hervilla <cupidreborn522@gmail.com>
2025-11-11 11:40:25 -0400

    app con clusters

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+// ecosystem.config.js  
+module.exports = {  
+  apps: [{  
+    name: 'api-webhook',  
+    script: 'dist/main.js',  
+    instances: 10,  // 10 procesos compartiendo el puerto 5001  
+    exec_mode: 'cluster',  
+    max_memory_restart: '1.5G',  
+    env: {  
+      NODE_ENV: 'production',  
+      PORT: 5001  // UN SOLO PUERTO  
+    }  
+  }]  
+};
